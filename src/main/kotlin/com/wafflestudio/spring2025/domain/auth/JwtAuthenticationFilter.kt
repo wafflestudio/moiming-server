64aef7310a227919da0d8fc3aaa3778765135591
Jisu Hong <jaylovegood@snu.ac.kr>
2026-01-17 00:21:22 +0900

    :pencil2: Fix public path

--- src/main/kotlin/com/wafflestudio/spring2025/domain/auth/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/wafflestudio/spring2025/domain/auth/JwtAuthenticationFilter.kt
@@ -56,5 +56,5 @@ class JwtAuthenticationFilter(
         pathMatcher.match("/api/v1/auth/**", path) ||
             pathMatcher.match("/swagger-ui/**", path) ||
             pathMatcher.match("/v3/api-docs/**", path) ||
-            pathMatcher.match("/api/actuator/health", path)
+            pathMatcher.match("/actuator/health", path)
 }
