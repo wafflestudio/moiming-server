96b827cc57d3e330266ff5746cd18a9640bd5605
정성원 <jwjeong2187@snu.ac.kr>
2026-02-26 13:59:30 +0900

    @JsonAlias 제거

--- src/main/kotlin/com/wafflestudio/spring2025/domain/registration/dto/request/DeleteRegistrationRequest.kt
+++ src/main/kotlin/com/wafflestudio/spring2025/domain/registration/dto/request/DeleteRegistrationRequest.kt
@@ -1,14 +1,11 @@
 package com.wafflestudio.spring2025.domain.registration.dto.request
 
-import com.fasterxml.jackson.annotation.JsonAlias
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "이벤트 취소 요청")
 data class DeleteRegistrationRequest(
     @Schema(description = "비회원 이름")
-    @JsonAlias("guest_name")
     val guestName: String? = null,
     @Schema(description = "비회원 이메일")
-    @JsonAlias("guest_email")
     val guestEmail: String? = null,
 )
