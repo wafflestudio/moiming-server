3b312b680c7e869585e4327f0ae9edea88806af8
정성원 <jwjeong2187@snu.ac.kr>
2026-02-26 13:57:58 +0900

    불필요한 migration 파일 제거

--- src/main/resources/db/migration/V13__remove_cancel_registration_token_purpose.sql
+++
@@ -1,9 +0,0 @@
--- Remove cancel tokens before dropping enum value
-DELETE FROM registration_tokens
-WHERE purpose = 'CANCEL';
-
--- Drop CANCEL from enum
-ALTER TABLE registration_tokens
-    MODIFY purpose ENUM(
-    'CHANGE_VOTE'
-    ) NOT NULL;
