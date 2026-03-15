f52e405471a4241fe29eb7d201dc3ddd5b4ff66c
Jisu Hong <jaylovegood@snu.ac.kr>
2026-03-15 21:55:02 +0900

    :card_file_box: event 삭제 전에 registration 삭제 (fk)

--- src/main/resources/db/migration/V14__make_registration_ends_at_not_null.sql
+++ src/main/resources/db/migration/V14__make_registration_ends_at_not_null.sql
@@ -1,2 +1,3 @@
+DELETE r FROM registrations r INNER JOIN events e ON r.event_id = e.id WHERE e.registration_ends_at IS NULL;
 DELETE FROM events WHERE registration_ends_at IS NULL;
 ALTER TABLE events MODIFY registration_ends_at DATETIME(6) NOT NULL;
