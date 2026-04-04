d6b660cf887135eff8c163d5312dea9c2803a3db
Jisu Hong <jaylovegood@snu.ac.kr>
2026-04-05 00:23:09 +0900

    :pushpin: version upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import java.util.Properties
 
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
+    kotlin("jvm") version "2.2.0"
+    kotlin("plugin.spring") version "2.2.0"
     id("org.springframework.boot") version "3.5.5"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.flywaydb.flyway") version "9.22.3"
